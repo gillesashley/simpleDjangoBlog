51bbcb850ea9660dd3a3327d2a3b5c7d3694364c
gillesashley <xylaray37@gmail.com>
2024-01-14 21:21:47 +0000

    added likes field to Post model, added Comment model.

--- posts/models.py
+++ posts/models.py
@@ -2,14 +2,25 @@ from django.db import models
 from django.conf import settings
 
 
-# Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    body = models.TextField()
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='comment_likes', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body
